5a08ecf388925afbf75922403aba0de926173d13
JSlovak <jasmine.slovak@gmail.com>
2017-03-14 19:23:50 -1000

    Tests Passed (Although, I'm still digesting whyyyyyy).

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
+
 module.exports = function() {
-  
+
+  var secretNumber = Math.floor((Math.random() * 1000001));
+
+  function repeatNumber(){
+    return secretNumber;
+  }
+
+return repeatNumber;
+
 };
